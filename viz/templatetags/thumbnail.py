902addf5570d77da505ac0edd793275718bb0bdb
Alex Hornstein <alex@artiswrong.com>
2015-03-11 00:45:42 +0000

    latest push

--- viz/templatetags/thumbnail.py
+++ viz/templatetags/thumbnail.py
@@ -1,17 +1,25 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django import template
 from viz.models import Viz, Photo
+from django.conf import settings
 register = template.Library()
+import logging
+log = logging.getLogger(__name__)
 
 @register.inclusion_tag('viz/thumbnail.html')
 def thumbnail(viz, nextPage=None, vizType=None):
     try:
-        photo=Photo.objects.filter(viz=viz)[:1]  #get the main image associated with this viz, and use it as the photo
+        photo=Photo.objects.filter(viz=viz)[:1]  #get the main image associated with this viz, and use it as the photo                                                              
         photo=photo[0]
-    except:
+        log.debug("photo is %s, url is %s" %(photo, photo.file.url))
+        thumbnailPhotoURL=photo.file.url.replace(settings.MEDIA_URL, "http://cubetube.imgix.net/")
+        log.debug("fixed photo url is %s" % thumbnailPhotoURL)
+    except Exception,e:
         photo=None
+        thumbnailPhotoURL=None
+        log.debug(e)
     if nextPage is not None:
-            return { 'viz' : viz, 'thumbnailPhoto' : photo, 'nextPage' : nextPage, 'vizType' : vizType }
+            return { 'viz' : viz, 'thumbnailPhoto' : photo, 'nextPage' : nextPage, 'vizType' : vizType, 'thumbnailPhotoURL' :thumbnailPhotoURL}
     else:
-        return { 'viz' : viz, 'thumbnailPhoto' : photo }
+        return { 'viz' : viz, 'thumbnailPhoto' : photo , 'thumbnailPhotoURL' :thumbnailPhotoURL}
 
