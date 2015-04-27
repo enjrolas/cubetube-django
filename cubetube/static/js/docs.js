38b3287fee928b85b2601d39a1e50dafb5ec350d
Tim <timothy.w.holman@gmail.com>
2015-04-27 16:56:41 -0400

    sticky ness of sidebar

--- cubetube/static/js/docs.js
+++ cubetube/static/js/docs.js
@@ -1,14 +1,23 @@
 var $sidebar = $('.docs-nav');
+var $docs    = $('.docs-main')
 var turningPoint = $sidebar.offset().top - 55; // added header height.
+var endingPoint = $docs.offset().top + $docs.height() - $sidebar.height();// + ($sidebar.height() - window.innerHeight);
+
+console.log( endingPoint );
+
 if( $sidebar.length ) {
+
+    $sidebar.height
 
     var $window = $(window);
     $window.scroll(function() {
         var scrollPos = $window.scrollTop();
-        if( scrollPos > turningPoint ) {
-            $sidebar.addClass('fixed');
+        if( scrollPos > endingPoint ) {
+            $sidebar.removeClass('fixed').addClass('absolute');
+        } else if ( scrollPos > turningPoint ) {
+            $sidebar.addClass('fixed').removeClass('absolute');
         } else {
-            $sidebar.removeClass('fixed');
+            $sidebar.removeClass('fixed').removeClass('absolute');
         }
     })
 
