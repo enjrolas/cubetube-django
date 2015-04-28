5049dd31886c514b07878d1814215f42e98fc2e7
Tim <timothy.w.holman@gmail.com>
2015-04-28 13:17:54 -0400

    docs functionality

--- cubetube/static/js/docs.js
+++ cubetube/static/js/docs.js
@@ -7,7 +7,7 @@ if( $sidebar.length ) {
 
     $sidebar.height
     turningPoint = $sidebar.offset().top - 55; // added header height.
-    endingPoint = $docs.offset().top + $docs.height() - $sidebar.height();// + ($sidebar.height() - window.innerHeight);
+    endingPoint = $docs.offset().top + $docs.height() - $sidebar.height();
 
     var $window = $(window);
     $window.scroll(function() {
@@ -22,4 +22,18 @@ if( $sidebar.length ) {
     })
 
     // Set height of sidebar & handle resizes
+
+    // Click events on the sidebar
+    $('.docs-nav li').click(function() {
+
+        var $this = $(this);
+        var className = $(this).attr('class');
+        className = jQuery.trim(className.split('bolder').join(''));
+
+        var moveTo = $('#' + className);
+        $('html, body').animate({
+                scrollTop: moveTo.offset().top - 80
+        }, 500);
+    });
+
 }
