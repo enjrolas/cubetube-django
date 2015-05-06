3d5798abfb27b617f4a6c0b79829ec5a8bccd446
Tim <timothy.w.holman@gmail.com>
2015-05-05 23:26:16 -0400

    states

--- viz/static/js/create.js
+++ viz/static/js/create.js
@@ -4,4 +4,16 @@ $(function(){
     formatCode();
     setHeight();
 
+    // Switch states
+    $('.switch-part').click(function() {
+
+        var $this = $( this );
+        var $box = $this.closest('.switch-box');
+
+        $on =  $box.find('.on');
+        $off = $box.find('.off');
+
+        $on.removeClass('on').addClass('off');
+        $off.removeClass('off').addClass('on');
+    })
 });
