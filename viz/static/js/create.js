5de4cb3d99a3c485c03f7e886d4cdc773c55b875
Tim <timothy.w.holman@gmail.com>
2015-05-05 23:36:16 -0400

    editing in create

--- viz/static/js/create.js
+++ viz/static/js/create.js
@@ -16,4 +16,25 @@ $(function(){
         $on.removeClass('on').addClass('off');
         $off.removeClass('off').addClass('on');
     })
+
+    $('.run-sketch').click(function() {
+
+        var vizType = getEditVizType();
+        
+        if( vizType === 'L3D') {
+            translateCode(document.getElementById("code").value);
+            runSparkSketch();
+        } else if ( vizType === 'JS' ) {
+            runSketch();
+        }
+        
+    })
 });
+
+function getEditVizType() {
+
+    var type;
+    var $onElement = $('.viz-type .on');
+    var onVal = $onElement.html();
+    return onVal;
+}
