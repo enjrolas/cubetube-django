0d890ba22c5483af71f7948b6cde136caef4c50c
Alex Hornstein <alex@artiswrong.com>
2015-05-08 10:32:45 -0400

    putting up streaming.js and other websockets code -- not working yet, but it's conflicting with other stuff on the server

--- viz/static/js/streaming.js
+++ viz/static/js/streaming.js
@@ -1,9 +1,8 @@
 var localIP;
 var isStreaming=false;
-cube = null;
+var streaming = null;
 var frame=0;
 var r, g, b;
-var frameBuffer = new ArrayBuffer(512);
 var port = 2525;
 
 function getLocalIP(accessToken, coreID)
@@ -12,8 +11,8 @@ function getLocalIP(accessToken, coreID)
     $.get(url, function(data){
 	    localIP=data['result'];
 	    console.log(localIP);
-	    cube=new Cube("ws://" + localIP + ":" + port);
-	    cube.rate=5;
+	    streaming=new Streaming("ws://" + localIP + ":" + port);
+	    streaming.rate=5;
 	});
     
 }
@@ -23,68 +22,56 @@ function stream()
     getLocalIP(accessToken, coreID);
 }
 
-function bufferVoxels(red, green, blue)
-{
-    for(x=0;x<8;x++)
-	for(y=0;y<8;y++)
-	    for(z=0;z<8;z++)
-		frameBuffer[z*64+y*8+x]=((red[x][y][z] >> 5) << 5) | ((green[x][y][z] >> 5) << 2) | (blue[x][y][z] >> 6); 
-    if(frame===1)
-	console.log(frameBuffer);
-    frame++;
-
-}
-
-function Cube(address) {
+function Streaming(address) {
     this.clearToSend = true;
     this.rate = 1000;
     this.size = 8; // TODO support 16^3
     this.frameSize = 512;
+    var frameBuffer = new ArrayBuffer(512);
 
     // open connection
     this.ws = new WebSocket(address);
     console.log("Connecting!");
-
-    var cube = this;
+    //    var cube = this;
 
     this.ws.onclose = function() {
-        if(cube.onclose !== undefined) {
-            cube.onclose(cube);
+        if(this.onclose !== undefined) {
+            this.onclose(this);
         }
     };
 
     this.ws.onopen = function() {
-        if(cube.onopen !== undefined) {
-            cube.onopen(cube);
+        if(streaming.onopen !== undefined) {
+            this.onopen(this);
         }
 
-        cube.refresh();
+        this.refresh();
     };
 
     this.ws.onmessage = function(evt) {
         var msg = evt.data;
         console.log("got msg: " + msg);
 
-        if(parseInt(msg) == cube.frameSize) {
-            cube.clearToSend = true;
+        if(parseInt(msg) == this.frameSize) {
+            this.clearToSend = true;
 	    console.log("clear to send");
         }
     };
 }
 
-Cube.prototype.onopen= function() {}
-Cube.prototype.onclose= function() {}
-Cube.prototype.onrefresh= function() {}	
-Cube.prototype.refresh= function() {
-    var cube = this;
+Streaming.prototype.onopen= function() {}
+Streaming.prototype.onclose= function() {}
+Streaming.prototype.onrefresh= function() {}	
+Streaming.prototype.refresh= function() {
+    //    var cube = this;
     
     if(this.clearToSend && this.ws.bufferedAmount == 0) {
 	if(this.onrefresh !== undefined) {
 	    this.onrefresh(this);
 	}
-	console.log("sent frame "+frame);
-	console.log(frameBuffer);
-	this.ws.send(frameBuffer);
+	//	console.log("sent frame "+frame);
+	console.log(this.frameBuffer);
+	this.ws.send(this.frameBuffer);
 	this.clearToSend = false; // must get reply before sending again
 	
 	setTimeout(function() { cube.refresh(); }, cube.rate);
@@ -93,3 +80,16 @@ Cube.prototype.refresh= function() {
 	setTimeout(function() { cube.refresh(); }, 5);
     }
 }
+
+Streaming.prototype.bufferVoxels = function(red, green, blue)
+{
+    var frameView = new Uint8Array(this.frameBuffer);
+    for(var x=0;x<8;x++)
+	for(var y=0;y<8;y++)
+	    for(var z=0;z<8;z++)
+		this.frameBuffer[z*64+y*8+x]=(((red[x][y][z] >> 5) << 5) | ((green[x][y][z] >> 5) << 2) | (blue[x][y][z] >> 6)); 
+    frame++;
+
+}
+
+
