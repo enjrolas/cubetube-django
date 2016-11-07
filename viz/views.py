ca3503a89636bea0ce53de0dd8391a90802b1bcd
wmoecke <wmoecke@gmail.com>
2016-11-07 14:59:22 -0200

    still no luck

--- viz/views.py
+++ viz/views.py
@@ -964,7 +964,7 @@ def viz_flashed(request):
     next_month  = int(month) + 1 if (int(month) < 12) else 1
 
     #vizs=Viz.objects.filter(vizType="L3D").exclude(published=False).filter(lastFlashed__gte=datetime.date(today.year, int(month), 1), lastFlashed__lt=datetime.date(next_year, next_month, 1))
-    vizs=Viz.objects.filter(vizType="L3D").exclude(published=False).filter(lastFlashed__gte=("%s-%s-%s" % (today.year, int(month), 1)), lastFlashed__lt=("%s-%s-%s" % (next_year, next_month, 1)))
+    vizs=Viz.objects.filter(vizType="L3D").exclude(published=False).filter(lastFlashed__gte=datetime.date(today.year, int(month), 1).date(), lastFlashed__lt=datetime.date(next_year, next_month, 1).date())
     log.debug("SQL QUERY: %s" % vizs.extra(select={'day':'DATE_FORMAT(lastFlashed,\'%%d\')','fmtLastFlashed':'DATE_FORMAT(lastFlashed,\'%%m/%%d/%%Y\')'}).values('day').annotate(count=Count('pk')).values('fmtLastFlashed','count').query.__str__())
     grouped_query=list(vizs.extra(select={'day':'DATE_FORMAT(lastFlashed,\'%%d\')','fmtLastFlashed':'DATE_FORMAT(lastFlashed,\'%%m/%%d/%%Y\')'}).values('day').annotate(count=Count('pk')).values('fmtLastFlashed','count'))
     series = []
